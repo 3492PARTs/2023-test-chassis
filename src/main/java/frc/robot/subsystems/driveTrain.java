ceac124885311a3a66a21478e106f4742442fb83
Cameron <ckiw23@gmail.com>
2023-01-23 11:14:12 -0500

    definetly cordinate errors but should now be able to follow a trajectory

--- src/main/java/frc/robot/subsystems/driveTrain.java
+++ src/main/java/frc/robot/subsystems/driveTrain.java
@@ -4,7 +4,15 @@
 
 package frc.robot.subsystems;
 
+import java.util.HashMap;
+import java.util.function.BiConsumer;
+import java.util.function.Supplier;
+
 import com.kauailabs.navx.frc.AHRS;
+import com.pathplanner.lib.PathPlannerTrajectory;
+import com.pathplanner.lib.auto.PIDConstants;
+import com.pathplanner.lib.auto.RamseteAutoBuilder;
+import com.pathplanner.lib.commands.PPRamseteCommand;
 import com.revrobotics.CANSparkMax;
 import com.revrobotics.CANSparkMaxLowLevel.MotorType;
 
@@ -12,12 +20,17 @@ import PARTSlib2023.PARTS.frc.Utils.Interfaces.beanieDriveTrain;
 import PARTSlib2023.PARTS.frc.Utils.sensors.wheelLinearDistance;
 import PARTSlib2023.PARTS.frc.commands.joystickDrive;
 import edu.wpi.first.math.Pair;
+import edu.wpi.first.math.controller.PIDController;
+import edu.wpi.first.math.controller.RamseteController;
+import edu.wpi.first.math.controller.SimpleMotorFeedforward;
 import edu.wpi.first.math.estimator.DifferentialDrivePoseEstimator;
 import edu.wpi.first.math.geometry.Pose2d;
 import edu.wpi.first.math.kinematics.DifferentialDriveKinematics;
+import edu.wpi.first.math.kinematics.DifferentialDriveWheelSpeeds;
 import edu.wpi.first.wpilibj.motorcontrol.MotorControllerGroup;
 import edu.wpi.first.wpilibj.smartdashboard.Field2d;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import frc.robot.RobotContainer;
 
@@ -37,6 +50,11 @@ public class driveTrain extends beanieDriveTrain {
   DifferentialDriveKinematics dKinematics = new DifferentialDriveKinematics((Double) 20.0); //tbd
   DifferentialDrivePoseEstimator dEstimator = new DifferentialDrivePoseEstimator(dKinematics, getRotation(), leftDistance(), rightDistance(), null);
   private static driveTrain mDriveTrain = new driveTrain();
+    private static double kv;
+    private static double ka;
+    private static double ks;
+    public  static HashMap<String, Command> eventMap = new HashMap<>();
+
     
 
   /** Creates a new driveTrain. */
@@ -80,6 +98,18 @@ public class driveTrain extends beanieDriveTrain {
         return wheelAverage(leftWheels);
     }
 
+    public Supplier<Pose2d> getPoseSupplier(){
+        Supplier<Pose2d> s = () -> dEstimator.getEstimatedPosition();
+        return s;
+    }
+
+    public DifferentialDriveKinematics getKinematics() {
+        return dKinematics;
+    }
+
+    public void resetPose(Pose2d newPose){
+        dEstimator.resetPosition(getRotation(), leftDistance(), rightDistance(), newPose);
+    }
 
 
 
@@ -96,6 +126,15 @@ public class driveTrain extends beanieDriveTrain {
         
     }
 
+    public BiConsumer<Double, Double> getBiConsumer() {
+        BiConsumer<Double, Double> biC = (leftVoltage, rightVoltage) -> {
+          leftControllerGroup.setVoltage(leftVoltage);
+          rightControllerGroup.setVoltage(rightVoltage);
+          super.mDrive.feed();  
+        };
+        return biC;
+    }
+
     @Override
     public void teleopSetup() {
         // TODO Auto-generated method stub
@@ -109,6 +148,43 @@ public class driveTrain extends beanieDriveTrain {
         m_field.setRobotPose(dEstimator.getEstimatedPosition());
 
     }
+
+
+    public double getLeftVelocity(){
+        return (leftWheels[0].getVelocity() + leftWheels[1].getVelocity())/2;
+        }
+
+    public double getRightVelocity(){
+        return (rightWheels[0].getVelocity() + rightWheels[1].getVelocity())/2;
+        }
+    
+
+
+
+
+    
+  public Supplier<DifferentialDriveWheelSpeeds> getWheelSpeedSupplier(){
+    Supplier<DifferentialDriveWheelSpeeds> s = () -> new DifferentialDriveWheelSpeeds((getLeftVelocity() * 6*Math.PI)/8.01, (getRightVelocity()* 6*Math.PI)/8.01);
+    return s;
+ }
+ public Command followTrajectoryCommand(PathPlannerTrajectory traj, boolean isFirstPath) {
+
+     PPRamseteCommand controller1 = new PPRamseteCommand(
+        traj,
+        driveTrain.getInstance().getPoseSupplier(),
+        new RamseteController(),
+        new SimpleMotorFeedforward(ks, kv, ka),
+        driveTrain.getInstance().getKinematics(),
+        driveTrain.getInstance().getWheelSpeedSupplier(),
+        new PIDController(1, 0, 0), // Left controller. Tune these values for your robot. Leaving them 0 will only use feedforwards.
+        new PIDController(1, 0, 0),
+        driveTrain.getInstance().getBiConsumer(),
+        this
+        );
+
+    return controller1;
+ }
+
 
 
 
